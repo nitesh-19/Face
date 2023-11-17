f9eb0f3de4a02ffb3b223749475247ba6292a8c9
nitesh-19 <niteshgohil25@gmail.com>
2023-11-17 10:54:19 +0530

    testing functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #pragma ide diagnostic ignored "EndlessLoop"
 
+#include "iostream"
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 //#include <opencv2/core/utility.hpp>
@@ -8,21 +10,33 @@ using namespace std;
 
 int main() {
 
-//    string path = "C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Recognition-Cpp\\man.jpg";
-    VideoCapture cap(0);
-//    Mat img = imread(path);
-    Mat img;
+    string path = "C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face\\man.jpg";
+    //VideoCapture cap(0);
+    Mat img = imread(path);
+    //Mat img;
     Mat img_gray;
     Mat img_blur;
-    while (true) {
-        cap.read(img);
-        cvtColor(img, img_gray, COLOR_BGR2GRAY);
-        GaussianBlur(img, img_blur, Size(7, 7), 5, 0);
-        imshow("Image", img_blur);
-        cv::waitKey(1);
-        //fewacf
+    Mat img_canny;
 
-    }
+    Mat resized_img;
+
+    Mat cropped_img;
+
+
+    cout << img.size << endl;
+    //     cap.read(img);
+    cvtColor(img, img_gray, COLOR_BGR2GRAY);
+    GaussianBlur(img, img_blur, Size(7, 7), 2, 0);
+    Canny(img_blur, img_canny, 50, 150);
+    Rect roi(100, 100, 150, 150);
+//    cropped_img = img_gray(roi);
+    circle(img, Point(100, 100), 155, Scalar(0, 69, 255));
+    imshow("Image", img);
+
+
+    cv::waitKey(0);
+    imshow("Image", img_canny);
+    cv::waitKey(0);
 
 
 }
