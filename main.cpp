262207ea69e6d671f9999e37c93173834524ac0f
nitesh-19 <niteshgohil25@gmail.com>
2023-11-18 17:56:27 +0530

    Added mouse coordinate capture

--- main.cpp
+++ main.cpp
@@ -1,49 +1,37 @@
-#pragma ide diagnostic ignored "EndlessLoop"
-
 #include "iostream"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-//#include <opencv2/core/utility.hpp>
+#include <opencv2/core/types.hpp>
+
 using namespace cv;
 using namespace std;
+Point2f param[4] = {{0, 0},
+                    {0, 0},
+                    {0, 0},
+                    {0, 0}};
+int clicks = 0;
 
-void event(int event, int x, int y) {
-    if (event == EVENT_LBUTTONUP) {
+static void event(int event, int x, int y) {
+    if (event == EVENT_LBUTTONUP and clicks < 4) {
         cout << "Mouse click" << endl;
         cout << x << " " << y << endl;
-
+        param[clicks] = {float(x), float(y)};
+        cout << param[clicks] << endl;
+        clicks++;
     }
 }
 
+
 int main() {
-
-    string path = "C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face\\man.jpg";
-    //VideoCapture cap(0);
+    string path = "C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face\\grill.jpg";
+    param[2] = {5, 2};
     Mat img = imread(path);
-    //Mat img;
-    Mat img_gray;
-    Mat img_blur;
-    Mat img_canny;
-
-    Mat resized_img;
-
-    Mat cropped_img;
-
-
     cout << img.size << endl;
-    //     cap.read(img);
-    cvtColor(img, img_gray, COLOR_BGR2GRAY);
-    GaussianBlur(img, img_blur, Size(7, 7), 2, 0);
-    Canny(img_blur, img_canny, 50, 150);
-    Rect roi(100, 100, 150, 150);
-//    cropped_img = img_gray(roi);
-    circle(img, Point(100, 100), 155, Scalar(0, 69, 255));
+    cout << param[2] << endl;
     imshow("Image", img);
-
     setMouseCallback("Image", reinterpret_cast<MouseCallback>(event));
     cv::waitKey(0);
-    imshow("Image", img_canny);
-    cv::waitKey(0);
+    cout << param[0] << endl;
 
 
 }
