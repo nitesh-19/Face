94a0f7d074f7de32c359f6fe8808c0bc508c3f45
nitesh-19 <niteshgohil25@gmail.com>
2023-11-20 20:14:29 +0530

    Version commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <opencv2/core/types.hpp>
 #include <windows.h>
 
-
 using namespace cv;
 using namespace std;
 Point2f captured_coordinates[4] = {{0, 0},
@@ -12,6 +11,8 @@ Point2f captured_coordinates[4] = {{0, 0},
                                    {0, 0},
                                    {0, 0}};
 int clicks = 0;
+Mat img;
+Mat result_image;
 
 float get_distance(Point2f (&p1), Point2f (&p2)) {
     float x = p2.x - p1.x;
@@ -27,37 +28,38 @@ static void event(int event, int x, int y) {
         cout << captured_coordinates[clicks] << endl;
         clicks++;
     } else if (clicks > 3) {
-        return;
+        destroyWindow("Image");
     }
-
 }
 
-void button(int state) {
-    cout << state << endl;
+void trackbar_slider(int state) {
+    if (state == 0) {
+        // Handle division by zero, or set a minimum value for state
+        cout << "state 0";
+    }
+    double scaleFactor = state / 250;
+    Mat scaled_image;
+    resize(img, result_image, Size(), scaleFactor, scaleFactor, INTER_LINEAR);
+    imshow("Result", result_image);
 }
 
 
 int main() {
-
     int screen_width = (int) GetSystemMetrics(SM_CXSCREEN);
     int screen_height = (int) GetSystemMetrics(SM_CYSCREEN);
+
     string path = "C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face\\grill.jpg";
-    Mat img = imread(path);
-    Mat matrix, result_image;
+    img = imread(path);
+    Mat matrix;
+
     float h = img.size[0];
     float w = img.size[1];
-    cout << img.size << endl;
-    int a;
-    int *p = &a;
-    *p = 500;
-    cout << "Click on the four corners of the image that you want to transform." << endl;
-    resize(img, img, Size(), 0.8, 0.8, INTER_LINEAR);
+    int a = 100;
+
+
     imshow("Image", img);
-    createTrackbar("faw", "Image", p, 500, reinterpret_cast<TrackbarCallback>(button));
     setMouseCallback("Image", reinterpret_cast<MouseCallback>(event));
     cv::waitKey(0);
-//    namedWindow("Frame");
-//    createButton("but", reinterpret_cast<ButtonCallback>(button), nullptr, QT_PUSH_BUTTON, false);
 
     float avg_width = (get_distance((captured_coordinates)[0], (captured_coordinates)[1]) +
                        get_distance((captured_coordinates)[2], (captured_coordinates)[3])) / 2;
@@ -68,9 +70,15 @@ int main() {
                               {avg_width, 0},
                               {0,         avg_height},
                               {avg_width, avg_height}};
+    namedWindow("Result");
     matrix = getPerspectiveTransform(captured_coordinates, destination);
     warpPerspective(img, result_image, matrix, Point(avg_width, avg_height));
+//    img = result_image;
     imshow("Result", result_image);
+    createTrackbar("ScaleX", "Result", &a, 500, reinterpret_cast<TrackbarCallback>(trackbar_slider));
+
+//    createTrackbar("Scale Y", "Image", p, 100, reinterpret_cast<TrackbarCallback>(trackbar_slider));
     waitKey(0);
+    return 0;
 
 }
